216a9ec6bb1e56a42acfc6bf2cf551ce8f92441f
Rehan Abbu <rehanhabbu@gmail.com>
2025-03-30 12:14:57 -0400

    Update main.tsx

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -3,8 +3,7 @@ import App from "./App";
 import "./index.css";
 import { queryClient } from "./lib/queryClient";
 import { QueryClientProvider } from "@tanstack/react-query";
-import { Toaster } from "@/components/ui/toaster";
-
+import { Toaster } from "./components/ui/toaster";
 // Register service worker for PWA support
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', () => {
