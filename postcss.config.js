6da1bca17aac91ba5a5c9d1461b16e3917d5e8d8
Rehan Abbu <rehanhabbu@gmail.com>
2025-03-30 12:20:29 -0400

    Update postcss.config.js

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,15 @@
-export default {
+module.exports = {
   plugins: {
     tailwindcss: {},
-    autoprefixer: {},
-  },
-}
+    // Only use autoprefixer if it's available
+    ...((() => {
+      try {
+        require.resolve('autoprefixer');
+        return { autoprefixer: {} };
+      } catch (e) {
+        console.warn('Autoprefixer not found, CSS will not be prefixed');
+        return {};
+      }
+    })())
+  }
+};
